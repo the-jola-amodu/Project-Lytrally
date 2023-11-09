9630be10c8fe3e7c679e9f3908f5100b1fea1a10
Jolaoluwa Amodu <jola47amodu@gmail.com>
2023-11-09 14:50:55 +0100

    Update app.py
    Using WhiteNoise to serve stylesheets, to fix deployment issues

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from whitenoise import WhiteNoise
 
 app = Flask(__name__)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="/static", prefix="/static/style")
 
 @app.route('/')
 def index():
