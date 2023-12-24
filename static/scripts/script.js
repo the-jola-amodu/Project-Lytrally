a035b9144123b74cd7c7853c2ef09bfef4573e63
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2023-12-24 18:57:05 +0100

    Implement singin and signup card for mobile view

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,28 +1,92 @@
 // This code opens and closes the dropdown
-let hambuger = document.getElementById('openNav');
-let navDisplay = document.querySelector('.navDisplay');
+let hambuger = document.getElementById("openNav");
+let navDisplay = document.querySelector(".navDisplay");
+let sScreen = document.querySelector(".s-screen");
+let landingPage = document.querySelector(".landingPage");
+sScreen.style.zIndex = "9999";
+var overlay = document.createElement("div");
+overlay.classList.add("overlay");
+overlay.style.display = "none";
+overlay.style.opacity = "0.9";
+overlay.style.backdropFilter = "blur(3px)";
+overlay.style.zIndex = "1";
+overlay.style.top = "0";
+overlay.style.left = "0";
+overlay.style.right = "0";
+overlay.style.bottom = "0";
+overlay.style.position = "fixed";
+overlay.style.background = 'rgba(0,0,0,0.5)';
+landingPage.appendChild(overlay);
 let openNavBar = () => {
-    navDisplay.style.height = "300px";
+  if (navDisplay.style.display === "none") {
+    navDisplay.style.height = "400px";
     navDisplay.style.paddingBlock = "32px";
-}
-let closeNavBar = () => {
+    overlay.style.display = "block";
+    navDisplay.style.borderRadius = "10px";
+    navDisplay.style.display = "flex";
+    signinSignupContainer.style.display = "none";
+  } else {
+    navDisplay.style.display = "none";
+    overlay.style.display = "none";
     navDisplay.style.height = "0px";
     navDisplay.style.paddingBlock = "0px";
+  }
+};
+// let closeNavBar = () => {
+//     navDisplay.style.display = "none";
+//     navDisplay.style.height = "0px";
+//     navDisplay.style.paddingBlock = "0px";
+// }
+
+// The code below control how the display of the signin and signup card
+let signInLink = document.getElementById("signIn");
+let signUpLink = document.getElementById("signUp")
+let signinSignupContainer = document.getElementById("signIn_signUp");
+let signinSigiupHeader = document.getElementById("signin_sigiup_header");
+let googleBtn = document.getElementById("google_btn");
+let facebookBtn = document.getElementById("facebook_btn");
+let emailInput = document.getElementById("email");
+let passwordInput = document.getElementById("password");
+let rememberMe = document.querySelector(".check_forgot");
+let forgotPassword = document.getElementById("forgotPassword");
+let submitBtn = document.getElementById("submit");
+let noAccount = document.querySelector(".noAccount")
+
+let signIn_signUp = () =>
+{
+  navDisplay.style.display = "none";
+  overlay.style.display = "none";
+  navDisplay.style.height = "0px";
+  navDisplay.style.paddingBlock = "0px";
+  if (signinSignupContainer.style.display === "none")
+  {
+    signinSignupContainer.style.display = "block";
+  }
+  else
+  {
+    signinSignupContainer.style.display = "none";
+  }
 }
 
 // This code displays the features list
-let featuresDisplay = document.getElementById('featuresDisplay');
-let lists = document.getElementById('lists');
-let closeDisplay = document.getElementById('closeDisplay');
+let featuresDisplay = document.getElementById("featuresDisplay");
+let lists = document.getElementById("lists");
+let closeDisplay = document.getElementById("closeDisplay");
 let features = () => {
-    lists.style.height = "100%";
-}
+  lists.style.height = "100%";
+};
 let closeFeatures = () => {
-    lists.style.height = "0"
-}
+  lists.style.height = "0";
+};
 
-const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-const screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+const screenWidth =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
+const screenHeight =
+  window.innerHeight ||
+  document.documentElement.clientHeight ||
+  document.body.clientHeight;
 window.addEventListener("load", () => {
-    console.log
-})
+  console.log;
+});
