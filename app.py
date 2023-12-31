4c9c19e41832ca3b81583c54cdc45b0024190ca0
Jola Amodu <jola47amodu@gmail.com>
2023-12-31 05:09:56 +0100

    Added new inputs and validation, submission issues

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__, static_url_path='/static')
@@ -6,10 +6,20 @@ app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = "09b8049e0722d8956b107b38fa2eface"
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    form = RegistrationForm()
-    return render_template('index.html', form=form)
+    login_form = LoginForm()
+    registration_form = RegistrationForm()
+    if request.method == 'POST':
+
+        if login_form.submit1.data and login_form.validate():
+            flash(f"Welcome, new user!", 'success')
+            return redirect(url_for('index'))
+
+        if registration_form.submit2.data and registration_form.validate():
+            flash(f"Welcome, new user!", 'success')
+            return redirect(url_for('index'))
+    return render_template('index.html', form1=login_form, form2=registration_form)
 
 
 @app.route('/signup', methods=['GET', 'POST'])
--- forms.py
+++ forms.py
@@ -4,15 +4,20 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min=5)])
-    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
-    policy_agreement = BooleanField("I Agree with privacy and policy")
-    submit = SubmitField("Let's go!")
+    email = StringField("Registration Email", validators=[
+                        DataRequired(), Email()])
+    password = PasswordField("Registration Password", validators=[
+                             DataRequired(), Length(min=5)])
+    confirm_password = PasswordField("Confirm Password", validators=[
+                                     DataRequired(), EqualTo("password")])
+    policy_agreement = BooleanField(
+        "I Agree with privacy and policy", validators=[DataRequired()])
+    submit2 = SubmitField("Register!")
 
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min=5)])
-    remember_me = BooleanField("Remember Me")
-    submit = SubmitField("Sign in")
+    password = PasswordField("Password", validators=[
+                             DataRequired(), Length(min=5)])
+    remember_me = BooleanField("Remember Me", validators=[DataRequired()])
+    submit1 = SubmitField("Sign in")
