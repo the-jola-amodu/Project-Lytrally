8109c3fb7426a2e02fa88bd871bbb5e48c44188e
Jola Amodu <jola47amodu@gmail.com>
2024-03-20 03:14:45 +0100

    Reverting back to using signin/signup pages

--- app.py
+++ app.py
@@ -8,18 +8,7 @@ app.config['SECRET_KEY'] = "09b8049e0722d8956b107b38fa2eface"
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    login_form = LoginForm()
-    registration_form = RegistrationForm()
-    if request.method == 'POST':
-
-        if login_form.submit1.data and login_form.validate():
-            flash(f"Welcome, new user!", 'success')
-            return redirect(url_for('index'))
-
-        if registration_form.submit2.data and registration_form.validate():
-            flash(f"Welcome, new user!", 'success')
-            return redirect(url_for('index'))
-    return render_template('index.html', form1=login_form, form2=registration_form)
+    return render_template('index.html')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
