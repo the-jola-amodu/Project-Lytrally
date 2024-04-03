e8a07d5734bce09a5586b2e8a42c984f40183d88
Jola Amodu <jola47amodu@gmail.com>
2024-04-03 01:25:49 +0100

    Fixed database, logout button in account

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = "09b8049e0722d8956b107b38fa2eface"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'signin'
+login_manager.login_message_category = 'info'
 
 # Make sure "from flask_app import routes" remains below this comment, or a circular import occurs
 from flask_app import routes
--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-from flask_app import app
+from flask_app import app, db
 
 if __name__ == "__main__":
+
+    # Create all database tables
+    with app.app_context():
+        db.create_all()
+
     app.run(debug=True)
