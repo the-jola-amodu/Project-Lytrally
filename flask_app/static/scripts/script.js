59caa293dfd34fc8e9d580f92b303bd62d588c6c
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-04-08 15:41:45 +0100

    Implementing the update form design

--- flask_app/static/scripts/script.js
+++ flask_app/static/scripts/script.js
@@ -33,6 +33,7 @@ hambuger.addEventListener("click", () => {
     overlay.style.display = "block";
     navDisplay.style.borderRadius = "10px";
     navDisplay.style.display = "flex";
+    gearDropdown.style.display = "none";
     // signinSignupContainer.style.display = "none";
   } else {
     navDisplay.style.display = "none";
@@ -55,6 +56,36 @@ navLinks.forEach((link) => {
     navDisplay.style.height = "0px";
     navDisplay.style.paddingBlock = "0px";
   });
+});
+
+// This code toggles the geardropdown
+let gear = document.querySelector(".gear")
+let gearDropdown = document.querySelector(".gearDropdown");
+gear.addEventListener("click", () => {
+    if (gearDropdown.style.display === "none")
+    {
+        gearDropdown.style.display = "flex";
+    }
+    else {
+        gearDropdown.style.display = "none";
+    }
+})
+
+// This code toggles the notification on and off
+let toggle = document.querySelector(".toggle");
+let toogleContainer = document.querySelector(".notificationToggle");
+toggle.addEventListener("click", () => {
+  if (toggle.classList.contains("toggleOn")) {
+    toggle.classList.remove("toggleOn");
+    toogleContainer.classList.remove("toggleContainerOn");
+    toggle.classList.add("toggleOff");
+    toogleContainer.classList.add("toggleContainerOff")
+  } else {
+    toggle.classList.remove("toggleOff");
+    toogleContainer.classList.remove("toggleContainerOff");
+    toggle.classList.add("toggleOn");
+    toogleContainer.classList.add("toggleContainerOn");
+  }
 });
 
 heroPage_sm = document.querySelector(".heroPage");
