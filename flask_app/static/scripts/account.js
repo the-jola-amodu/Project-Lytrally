4c9fedee766b8685a0eaf8e4cbc70f1b686b4c8e
MichaelOmoniyi <oomoniyimichael18@gmail.com>
2024-04-13 09:11:49 +0100

    Implementing scripts for the user update form

--- flask_app/static/scripts/account.js
+++ flask_app/static/scripts/account.js
@@ -6,11 +6,16 @@ let prevPage = () => {
 // This code toggles the display of the update form
 let updateLink = document.querySelector(".update");
 let updateFormDisplay = document.querySelector(".profileForm");
-
+let closeForm = document.querySelector(".closeForm");
+// Displayys and undisplay the form
 updateLink.addEventListener("click", () => {
   if (updateFormDisplay.style.display === "none") {
     updateFormDisplay.style.display = "flex";
   } else {
     updateFormDisplay.style.display = "none";
   }
+});
+// Closes the form
+closeForm.addEventListener("click", () => {
+  updateFormDisplay.style.display = "none";
 });
